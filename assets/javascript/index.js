9f953d5901ae671124139b8dc9b9f12b5d3485bc
Nick Meleshchenko <nick.meleshchenko@gmail.com>
2020-06-11 09:46:54 +0300

    final

---
+++ assets/javascript/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.navbar-toggler').on('click', ()=>{
+        $('.nav-toggler').toggleClass('open');
+        $('.avatar').toggleClass('d-none');
+    })    
+  })
